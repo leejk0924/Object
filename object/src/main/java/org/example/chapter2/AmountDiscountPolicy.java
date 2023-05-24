917830b1bc54b631b3f5a1b03d2a20058326f32a
jk <jk@jkui-MacBookPro.local>
2023-05-24 22:07:48 +0900

    [2.객체지향 프로그래밍] 영화 예매 시스템 1

---
+++ object/src/main/java/org/example/chapter2/AmountDiscountPolicy.java
@@ -0,0 +1,18 @@
+package org.example.chapter2;
+
+import java.util.List;
+
+public class AmountDiscountPolicy extends DiscountPolicy{
+    private Money discountAmount;
+
+    public AmountDiscountPolicy(Money discountAmount, DiscountCondition ... conditions) {
+        super(conditions);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money getDiscountAmount(Screening screening) {
+        return discountAmount;
+    }
+
+}
---
+++ object/src/main/java/org/example/chapter2/Movie.java
@@ -0,0 +1,25 @@
+package org.example.chapter2;
+
+import java.time.Duration;
+
+public class Movie {
+    private String title;
+    private Duration duration;
+    private Money fee;
+    private DiscountPolicy discountPolicy;
+
+    public Movie(String title, Duration duration, Money fee, DiscountPolicy discountPolicy) {
+        this.title = title;
+        this.duration = duration;
+        this.fee = fee;
+        this.discountPolicy = discountPolicy;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public Money calculateMovieFee(Screening screening) {
+        return fee.minus(discountPolicy.calculateDiscountAmount(screening));
+    }
+}
